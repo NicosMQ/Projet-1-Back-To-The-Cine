a7543674ae8098f1edeaaca90f316ef6c31c8995
LUDOVIC SCELLES <ludovic.scelles@gmail.com>
2024-03-21 15:53:50 +0100

    modification page 'les-films' et ajout des images

---
+++ les-films.js
@@ -0,0 +1,50 @@
+let togg1 = document.getElementById("togg1");
+let hi1 = document.getElementById("hi1");
+togg1.addEventListener("click", () => {
+    if(getComputedStyle(hi1).display != "none"){
+    hi1.style.display = "none";
+    } else {
+    hi1.style.display = "flex";
+    }
+  })
+
+let togg2 = document.getElementById("togg2");
+let hi2 = document.getElementById("hi2");
+togg2.addEventListener("click", () => {
+    if(getComputedStyle(hi2).display != "none"){
+    hi2.style.display = "none";
+    } else {
+    hi2.style.display = "flex";
+    }
+  })
+
+  
+let togg3 = document.getElementById("togg3");
+let hi3 = document.getElementById("hi3");
+togg3.addEventListener("click", () => {
+    if(getComputedStyle(hi3).display != "none"){
+    hi3.style.display = "none";
+    } else {
+    hi3.style.display = "flex";
+    }
+  })
+
+let togg4 = document.getElementById("togg4");
+let hi4 = document.getElementById("hi4");
+togg4.addEventListener("click", () => {
+    if(getComputedStyle(hi4).display != "none"){
+    hi4.style.display = "none";
+    } else {
+    hi4.style.display = "flex";
+    }
+  })
+
+let togg5 = document.getElementById("togg5");
+let hi5 = document.getElementById("hi5");
+togg5.addEventListener("click", () => {
+    if(getComputedStyle(hi5).display != "none"){
+    hi5.style.display = "none";
+    } else {
+    hi5.style.display = "flex";
+    }
+  })
