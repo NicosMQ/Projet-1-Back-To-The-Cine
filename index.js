0f6b736d0e3903cb6fd04b8ca6fccafc6713fde0
Nicos MUNTU <nicos19@hotmail.fr>
2024-03-22 08:59:35 +0100

    modif dimensiens images index.html, ajout des images films pour effets js

---
+++ index.js
@@ -0,0 +1,83 @@
+const TopgGunDivId = document.querySelector('#filmCinqTest');
+
+TopgGunDivId.addEventListener('mouseover', function(){
+
+    TopgGunDivId.src = "./images/top-gun2eme.jpg"
+});
+
+
+const TopgGunDiv1Id = document.querySelector('#filmCinqTest');
+
+TopgGunDiv1Id.addEventListener('mouseleave', function(){
+
+    TopgGunDiv1Id.src = "images/Top-Gun-20110421120657.jpg"
+});
+
+// // // // // // 
+
+
+const PereNoelDivId = document.querySelector('#filmQuatreTest');
+
+PereNoelDivId.addEventListener('mouseover', function(){
+
+    PereNoelDivId.src = "images/pere noel ordure 2.jpg"
+});
+
+const PereNoelDiv1Id = document.querySelector('#filmQuatreTest');
+
+PereNoelDiv1Id.addEventListener('mouseleave', function(){
+
+    PereNoelDiv1Id.src = "images/pere noel ordure.jpg"
+});
+
+// // // // // // //
+
+const CamilleDivId = document.querySelector('#filmTroisTest');
+
+CamilleDivId.addEventListener('mouseover', function(){
+
+    CamilleDivId.src = "images/Camille Claudel 2 .jpg"
+});
+
+const CamilleDiv1Id = document.querySelector('#filmTroisTest');
+
+CamilleDiv1Id.addEventListener('mouseleave', function(){
+
+    CamilleDiv1Id.src = "images/camille claudel.jpg"
+});
+
+// // // // // // // 
+
+const ShinningDivId = document.querySelector('#filmDeuxTest');
+
+ShinningDivId.addEventListener('mouseover', function(){
+
+    ShinningDivId.src = "images/shinning 2.jpg"
+});
+
+const ShinningDiv1Id = document.querySelector('#filmDeuxTest');
+
+ShinningDiv1Id.addEventListener('mouseleave', function(){
+
+    ShinningDiv1Id.src = "images/shinning.jpg"
+});
+
+
+// // // // // // // // 
+
+const RetourDivId = document.querySelector('#filmUnTest');
+
+RetourDivId.addEventListener('mouseover', function(){
+
+    RetourDivId.src = "images/retour vers le futur 2 .webp"
+});
+
+const RetourDiv1Id = document.querySelector('#filmUnTest');
+
+RetourDiv1Id.addEventListener('mouseleave', function(){
+
+    RetourDiv1Id.src = "images/back-to-the-future-i2795.jpg"
+});
+
+
+
